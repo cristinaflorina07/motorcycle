146aacbcb32b915cf257b0c7fb25b3310d00ba10
Cristina07 <cristinaflorina24@gmail.com>
2024-08-10 00:18:32 +0300

    Added new CarService and CarController

---
+++ src/main/java/com/motorcycle/controller/CarController.java
@@ -0,0 +1,26 @@
+package com.motorcycle.controller;
+
+import com.motorcycle.entity.Car;
+import com.motorcycle.service.CarService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carController")
+public class CarController {
+    private  final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+    @GetMapping("/getCar")
+    public List<Car> getCar(){
+        return carService.getCar();
+    }
+//    @PostMapping
+//    public Car addCar (@RequestBody Car car ){
+//        return CarService.addCar(car);
+
+    }
+}
--- src/main/java/com/motorcycle/service/CarService.java
+++ src/main/java/com/motorcycle/service/CarService.java
@@ -1,7 +1,10 @@
 package com.motorcycle.service;
 
+import com.motorcycle.entity.Car;
 import com.motorcycle.repository.CarRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarService {
@@ -9,5 +12,11 @@ public class CarService {
 
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
+    }
+    public List<Car> getCar(){
+        return carRepository.findAll();
+    }
+    public Car addCar(Car car){
+        return carRepository.save(car);
     }
 }
